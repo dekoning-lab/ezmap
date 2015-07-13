ece240f4a60576b44c29e3108058e2c2eb3c086c
patrickcze <Bruiser12>
2015-07-13 11:47:45 -0600

    Created directory and project directory arguments and function to process command line arguments

--- PipelineDev(SLURM)/Scripts/arguments.py
+++ PipelineDev(SLURM)/Scripts/arguments.py
@@ -1,1 +1,22 @@
 __author__ = 'patrickczeczko'
+
+import argparse
+
+
+def parseCommandLineArguments():
+    parser = argparse.ArgumentParser()
+
+    # Required Arguments
+    parser.add_argument("-d", "--directory", type=str, required=True,
+                        help="Provide a complete path to a directory containing the files of Raw Sequence Information")
+    parser.add_argument("--projDir", type=str, required=True,
+                        help="Provide a complete path to a directory where all intermediate files and folders will "
+                             "be placed")
+
+    args = parser.parse_args()
+
+    allArgs = {}
+    allArgs['directory'] = args.directory
+    allArgs['projDir'] = args.projDir
+
+    return allArgs
