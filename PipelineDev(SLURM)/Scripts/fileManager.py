66a96851e8cfe24eb41fd33cff5c6cb16d27c03f
patrickcze <Bruiser12>
2015-07-13 11:50:19 -0600

    Added functions to create a folder of subfolders to house intermediate files during processing

--- PipelineDev(SLURM)/Scripts/fileManager.py
+++ PipelineDev(SLURM)/Scripts/fileManager.py
@@ -1,1 +1,32 @@
 __author__ = 'patrickczeczko'
+
+import os
+from Scripts.dataset import Dataset
+
+# Create Subdirectories for all the intermediate files
+def createSubFolders(projDir):
+    if os.path.exists(projDir):
+        print('Creating Sub directories in ' + projDir)
+        if not os.path.exists(projDir + '/1-Cleaning/'):
+            os.mkdir(projDir + '/1-Cleaning/')
+        if not os.path.exists(projDir + '/2-HumanMapping/'):
+            os.mkdir(projDir + '/2-HumanMapping/')
+        if not os.path.exists(projDir + '/3-UnmappedCollection/'):
+            os.mkdir(projDir + '/3-UnmappedCollection/')
+        if not os.path.exists(projDir + '/4-OrganismMapping/'):
+            os.mkdir(projDir + '/4-OrganismMapping/')
+        if not os.path.exists(projDir + '/5-RelativeAbundanceEstimation/'):
+            os.mkdir(projDir + '/5-RelativeAbundanceEstimation/')
+        if not os.path.exists(projDir + '/6-FinalResult/'):
+            os.mkdir(projDir + '/6-FinalResult/')
+        return True
+    return False
+
+# return a list of dataset objects corressponding to each file
+def getListOfOriginalFiles(fileDir,projDir):
+    origfiles = {}
+    for file in os.listdir(fileDir):
+        if file.endswith('.fq') or file.endswith('.fastq'):
+            root, ext = os.path.splitext(file)
+            origfiles[root] = (Dataset(file, fileDir,projDir))
+    return origfiles
