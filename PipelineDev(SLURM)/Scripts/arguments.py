4ebaec933a621419da15221c07d96bc1e9751639
patrickcze <Bruiser12>
2015-07-13 12:04:15 -0600

    Added 4 Arguments for PRINSEQ

--- PipelineDev(SLURM)/Scripts/arguments.py
+++ PipelineDev(SLURM)/Scripts/arguments.py
@@ -12,7 +12,19 @@ def parseCommandLineArguments():
     parser.add_argument("--projDir", type=str, required=True,
                         help="Provide a complete path to a directory where all intermediate files and folders will "
                              "be placed")
-
+    # PRINSEQ Arguments
+    parser.add_argument("-pof", "--prinseq-out_format", type=int, required=True,
+                        help="Output format"
+                             "1 (FASTA only), 2 (FASTA and QUAL), 3 (FASTQ), 4 (FASTQ and FASTA), 5 (FASTQ, FASTA and "
+                             "QUAL)")
+    parser.add_argument("-pmqs", "--prinseq-min_qual_score", type=int, required=True,
+                        help="Filter sequence with at least one quality score below min_qual_score")
+    parser.add_argument("-plm", "--prinseq-lc_method", type=str, required=True,
+                        help="Method to filter low complexity sequences - [dust, entropy]")
+    parser.add_argument("-plt", "--prinseq-lc_threshold", type=int, required=True,
+                        help="The threshold value used to filter sequences by sequence complexity. The dust method "
+                             "uses this as maximum allowed score and the entropy method as minimum allowed value. "
+                             "INT [0..100]")
 
     args = parser.parse_args()
 
