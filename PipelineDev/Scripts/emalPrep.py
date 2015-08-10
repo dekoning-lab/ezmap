de2bb2cffb37707404bad65a7d70e39d2616df13
patrickcze <Bruiser12>
2015-08-10 11:05:20 -0600

    Create dall required functions to run EMAL data prep, main, and post.

--- PipelineDev/Scripts/emalPrep.py
+++ PipelineDev/Scripts/emalPrep.py
@@ -1,1 +1,100 @@
 __author__ = 'patrickczeczko'
+
+import subprocess
+import os
+import Scripts.slurmScript as slurmScript
+
+# Generates bash script to launch all required jobs within job manager
+def generatePreScript(dataSets, projdir, configOptions, blastjobids):
+    print('Setting up jobs for Step 5...')
+
+    cwd = os.path.dirname(os.path.abspath(__file__)).strip('Scripts')
+
+    script = open(projdir + '5-RelativeAbundanceEstimation/emalPreScript.sh', 'w+')
+
+    slurmScript.getSBATCHSettings(script, 5, projdir + '5-RelativeAbundanceEstimation/', configOptions)
+
+    IDList = ''
+    for i in blastjobids:
+        IDList += ':' + str(i)
+
+    script.write('#SBATCH --dependency=afterany:' + IDList[1:] + '\n\n')
+
+    script.write(
+        'srun ' +
+        configOptions['python3-path'] + ' ' + cwd + 'tools/EMAL/EMAL-DataPrep.py ' +
+        configOptions['blastn-db-path'] + ' ' +
+        configOptions['emal-gi-taxid-nucldmp-path'] + ' ' +
+        '1' + ' ' +
+        projdir + '5-RelativeAbundanceEstimation/')
+
+    return 'emalPreScript.sh'
+
+
+def generateMainScript(dataSets, projdir, configOptions, emalPrejobids):
+    cwd = os.path.dirname(os.path.abspath(__file__)).strip('Scripts')
+
+    script = open(projdir + '5-RelativeAbundanceEstimation/emalScript.sh', 'w+')
+
+    slurmScript.getSBATCHSettings(script, 5, projdir + '5-RelativeAbundanceEstimation/', configOptions)
+
+    IDList = ''
+    for i in emalPrejobids:
+        IDList += ':' + str(i)
+
+    script.write('#SBATCH --dependency=afterany:' + IDList[1:] + '\n\n')
+
+    script.write(
+        'srun ' +
+        configOptions['python3-path'] + ' ' + cwd + 'tools/EMAL/EMAL-Main.py ' +
+        '-d ' + projdir + '4-OrganismMapping/ ' +
+        '-v -t ' + configOptions['slurm-max-num-threads'] + ' ' +
+        '-c ' + projdir.split('/')[-2] + '.gra ' +
+        '-a ' + configOptions['emal-acceptance-value'] + ' ' +
+        '-o ' + projdir + '5-RelativeAbundanceEstimation/')
+
+    return 'emalScript.sh'
+
+
+def generatePostScript(dataSets, projdir, configOptions, emalJobIDS):
+    cwd = os.path.dirname(os.path.abspath(__file__)).strip('Scripts')
+
+    script = open(projdir + '5-RelativeAbundanceEstimation/emalPostScript.sh', 'w+')
+
+    slurmScript.getSBATCHSettings(script, 5, projdir + '5-RelativeAbundanceEstimation/', configOptions)
+
+    IDList = ''
+    for i in emalJobIDS:
+        IDList += ':' + str(i)
+
+    script.write('#SBATCH --dependency=afterany:' + IDList[1:] + '\n\n')
+
+    script.write(
+        'srun ' +
+        configOptions['python3-path'] + ' ' + cwd + 'tools/EMAL/EMAL-Post.py ' +
+        '-f ' + projdir + '5-RelativeAbundanceEstimation/' + projdir.split('/')[-2] + '.gra ' +
+        '-v -t ' + configOptions['slurm-max-num-threads'] + ' ' +
+        '-c ' + projdir.split('/')[-2] + '-finalResult.csv ' +
+        '-o ' + projdir + '5-RelativeAbundanceEstimation/')
+
+    return 'emalPostScript.sh'
+
+
+# Launch job to run within job manager
+def processAllFiles(projDir, configOptions, dataSets, stepNum, scriptName):
+    print('Queueing step 5-' + str(stepNum) + '...')
+    numOfFiles = len(dataSets)
+    proc = subprocess.Popen(
+        ['sbatch', projDir + '5-RelativeAbundanceEstimation/' + scriptName],
+        stdout=subprocess.PIPE)
+
+    outs, errs = proc.communicate()
+    outs = str(outs).strip('b\'Submitted batch job ').strip('\\n')
+
+    jobIDS = []
+    jobIDS.append(int(outs))
+
+    if configOptions['slurm-test-only'] == 'yes':
+        jobIDS = []
+
+    return jobIDS
