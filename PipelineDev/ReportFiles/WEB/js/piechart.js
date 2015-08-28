bf4a9987980b559d77253d99f8baf318f5aec27c
patrickcze <Bruiser12>
2015-08-28 14:03:07 -0600

    Added all required web files for final report generation

---
+++ PipelineDev/ReportFiles/WEB/js/piechart.js
@@ -0,0 +1,52 @@
+var w = 444;
+var h = 444;
+var r = h/2;                           //radius
+color = d3.scale.category20c();     //builtin range of colors
+
+d3.csv("../../information/emal-tbl-2.csv", function(error, data) {
+    var vis = d3.select("#summedPieChart")
+    .append("svg:svg")              //create the SVG element inside the <body>
+    .data([data])                   //associate our data with the document
+    .attr("width", w)           //set the width and height of our visualization (these will be attributes of the <svg> tag
+    .attr("height", h)
+    .append("svg:g")                //make a group to hold our pie chart
+    .attr("transform", "translate(" + r + "," + r + ")")    //move the center of the pie chart from 0, 0 to radius, radius
+
+    var arc = d3.svg.arc()              //this will create <path> elements for us using arc data
+    .outerRadius(r);
+
+    var pie = d3.layout.pie()           //this will create arc data for us given a list of values
+    .value(function(d) { return d.GRA; });    //we must tell it out to access the value of each element in our data array
+
+    var arcs = vis.selectAll("g.slice")     //this selects all <g> elements with class slice (there aren't any yet)
+    .data(pie)                          //associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties) 
+    .enter()                            //this will create <g> elements for every "extra" data element that should be associated with a selection. The result is creating a <g> for every object in the data array
+    .append("svg:g")                //create a group to hold each slice (we will have a <path> and a <text> element associated with each slice)
+    .attr("class", "slice")
+    .attr('id',function(d, i) { return 'test-'+data[i].Family; })
+    .on("mouseover", mouseover)
+    .on("mouseleave", mouseleave);//allow us to style things in the slices (like text)
+
+    arcs.append("svg:path")
+        .attr("fill", function(d, i) { return color(i); } ) //set the color for each slice to be chosen from the color function defined above
+        .attr("d", arc)
+    //this creates the actual SVG path using the associated data (pie) with the arc drawing function
+
+});
+
+function mouseover(d) {
+    var name = this.getAttribute('id');
+    name = name.substring(0,name.indexOf('['));
+
+    var row = document.getElementById(name);
+    row.style.backgroundColor = '#ADD8E6'
+}
+
+function mouseleave(d) {
+    var name = this.getAttribute('id');
+    name = name.substring(0,name.indexOf('['));
+
+    var row = document.getElementById(name);
+    row.style.backgroundColor = '#ffffff'
+}
+
---
+++ PipelineDev/ReportFiles/WEB/js/report.js
@@ -0,0 +1,187 @@
+$(document).ready(function(){
+    getProjectInformation();
+    
+    var fullDate = new Date()
+    var twoDigitMonth = ((fullDate.getMonth().length+1) === 1)? (fullDate.getMonth()+1) : '0' + (fullDate.getMonth()+1);
+    var currentDate = fullDate.getDate() + "/" + twoDigitMonth + "/" + fullDate.getFullYear();
+    $('#DATE').text(currentDate);
+
+    $('#collapseOne').load("information/filelist.txt");
+    csvToTable("information/prinseq-tbl-1.csv",$('#prinseqTblOneDiv'),'prinseqTblOne', 'PRINSEQ Results')
+    csvToTable("information/bowtie2-tbl-1.csv",$('#humanMappingTblOneDiv'),'humanMappingTblOne', 'BowTie2 Results')
+    csvToTable("information/samtools-tbl-1.csv",$('#unamppedTblOneDiv'),'unamppedTblOne', 'SamTools Results')
+    csvToTable("information/blastn-tbl-1.csv",$('#blastTblOneDiv'),'blastTblOne', 'Blastn Results')
+    csvToTable("information/emal-tbl-1.csv",$('#emalTblOneDiv'),'emalTblOne', 'EMAL Results')
+    csvToTable("information/emal-tbl-2.csv",$('#emalTblTwoDiv'),'emalTblTwo', 'EMAL Family Results')
+
+    $('#collapseAll').on('click', function (e) {
+        $(".panel-collapse").collapse('hide');
+        $(".panelIcon").removeClass('fa-chevron-down');
+        $(".panelIcon").addClass('fa-chevron-right');
+    });
+
+    $('#uncollapseAll').on('click', function (e) {
+        $(".panel-collapse").collapse('show');
+        $(".panelIcon").removeClass('fa-chevron-right');
+        $(".panelIcon").addClass('fa-chevron-down');
+    });
+
+    $('#print').on('click', function(e){
+        window.print();
+    });
+
+    var resizeId;
+    $(window).resize(function() {
+        clearTimeout(resizeId);
+        resizeId = setTimeout(doneResizing, 250);
+    });
+    initializeGRAGraph();
+    
+    $('.panel').on('show.bs.collapse', function (e) {
+        $('#'+e.currentTarget.id+'Icon').removeClass('fa-chevron-righ').addClass('fa-chevron-down');
+    })
+    $('.panel').on('hide.bs.collapse', function (e) {
+        $('#'+e.currentTarget.id+'Icon').removeClass('fa-chevron-down').addClass('fa-chevron-right');
+    })
+    $('#panel7').on('show.bs.collapse', function (e) {
+        initializeFileDistGraph();
+    })
+    $('#panel7').on('hide.bs.collapse', function (e) {
+        $('#mappedTo-graph').html('');
+        //initializeFileDistGraph();
+    })
+    
+    $('#downloadFileMappingDist').on("click", saveFileMappingSVG);
+    $('#downloadGRA').on("click", saveGRASVG);
+    $.getScript( "WEB/js/piechart.js", function( data, textStatus, jqxhr ) {});
+});
+
+function initializeGRAGraph (){
+    $.getScript( "WEB/js/sequences.js", function( data, textStatus, jqxhr ) {});
+}
+
+function initializeFileDistGraph(){
+    $.getScript( "WEB/js/mappedTo.js", function( data, textStatus, jqxhr ) {});
+}
+
+function saveFileMappingSVG (){
+    try {
+        var isFileSaverSupported = !!new Blob();
+    } catch (e) {
+        alert("blob not supported");
+    }
+
+    $('#mappedTo-graph').find("svg").attr("title", "test2").attr("version", 1.1).attr("xmlns", "http://www.w3.org/2000/svg");
+    var innerHTML = $('#mappedTo-graph').html();
+
+    var blob = new Blob([innerHTML], {type: "image/svg+xml"});
+    saveAs(blob, "fileMappingDistribution.svg");
+}
+
+function saveGRASVG (){
+    try {
+        var isFileSaverSupported = !!new Blob();
+    } catch (e) {
+        alert("blob not supported");
+    }
+
+    $('#chart').find("svg").attr("title", "test2").attr("version", 1.1).attr("xmlns", "http://www.w3.org/2000/svg");
+    var innerHTML = '<svg'+$('#chart').html().split('<svg').pop();
+    console.log(innerHTML)
+    var blob = new Blob([innerHTML], {type: "image/svg+xml"});
+    saveAs(blob, "fileMappingDistribution.svg");
+}
+
+function getProjectInformation(){
+    var file = 'information/projInfo.txt'
+    $.get(file, function(data) {
+        var rows = data.split("\n");
+        for (i=0; i < rows.length;i++){
+            var row = rows[i].split('=');
+            if (i==0){
+                $('#projTitle').text(row[1]);
+            }
+            else if (i==1){
+                $('#date').text(row[1]);
+            }
+        }
+    });
+}
+
+function doneResizing(){
+    var chartString = '<div id="explanation" style="visibility: hidden;"><span id="organism"></span><br/><span id="percentage"></span><br/>Relative abundance</div>';
+    $('#chart').html(chartString);
+    initializeGRAGraph ();
+    $('#mappedTo-graph').html('');
+    initializeFileDistGraph();
+}
+
+function csvToTable(file, $table, id, caption) {
+    var table = $.get(file, function(data, dataSet) {
+
+        // start the table
+        var dataSet = [];
+        var head = [];
+        var count = 0;
+        // split into lines
+        var rows = data.split("\n");
+        // parse lines
+        rows.forEach( function getvalues(row) {
+            // split line into columns
+            var columns = row.split(",");
+            if (count == 0 && columns[0].length > 0){
+                head = columns
+            }
+            if (count > 0 && columns[0].length > 0){
+                for (i = 0; i < columns.length; i++) {
+                    start = columns[i].indexOf("[");
+                    if (start > 0){
+                        end = columns[i].indexOf("]");
+                        taxid = columns[i].substring(start+1,end);
+                        string = '[<a href="http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id='+taxid+'" target="_blank">'+taxid+'</a>'
+                        columns[i] = columns[i].replace('['+taxid,string);
+                    }
+                }
+                dataSet.push(columns);
+            }
+            count += 1;
+        });
+        var header = [];
+        head.forEach(function createHeader(col) {
+            dic = {};
+            dic["title"] = col ;
+            header.push(dic);
+        });
+        $table.html('<table class="table" id="'+id+'"></table>');
+        $table = $('#'+id);
+        if (id == 'emalTblTwo'){
+            $('#summedOverTitle').text('Genome Relative Abundance Summed Over '+header[1].title)
+        }
+        if (id == 'emalTblOne' || id == 'emalTblTwo'){
+            $table.DataTable( {
+                "data": dataSet,
+                "columns": header,
+                "paging":   true,
+                "info":     false,
+                "responsive": true,
+                "order": [[ 0, "desc" ]],
+                "fnCreatedRow": function( nRow, aData, iDataIndex ) {
+                    var name = aData[1]
+                    name = name.substring(0,name.indexOf('['))
+                    $(nRow).attr('id', 'test-'+name);
+                }
+            });
+        }
+        else{
+            $table.dataTable( {
+                "data": dataSet,
+                "columns": header,
+                "paging":   true,
+                "info":     false,
+                "responsive": true,
+            } ); 
+        }
+    });
+
+}
+
