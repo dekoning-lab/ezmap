14d669d5fae9146f9cc1c4246fb4ea08ed4ee7b3
patrickcze <patrick.cze@gmail.com>
2016-05-09 15:52:41 -0600

    Updated setup script to make the installation of dependencies more obvious.

--- EzMap/setup.sh
+++ EzMap/setup.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
+
+echo "Installing EzMap dependencies..."
 # Setup Script to install all required dependencies
 echo "Current Python Version Installed"
 python3 -c 'import sys; print(".".join(map(str, sys.version_info[:3])))'
 
+echo "Installing Biopython...\n"
 pip3 install biopython
-pip3 install scipy
-pip3 install numpy
 
+echo "Installing SciPy...\n"
+pip3 install scipy
+
+echo "Installing NumPy...\n"
+pip3 install numpy
