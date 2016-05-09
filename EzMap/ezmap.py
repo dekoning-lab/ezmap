333ea7b098d3b7383e10302076ca940cc5d8b7b1
patrickcze <patrick.cze@gmail.com>
2016-05-09 15:43:49 -0600

    Updated documentation. Pipeline will now check if there are files to process prior to creating output directories

--- EzMap/ezmap.py
+++ EzMap/ezmap.py
@@ -7,7 +7,7 @@
 # Link: http://lab.jasondk.io
 # Github:
 #
-# Additrional documentation can be found on the github page.
+# Additional documentation can be found on the github page.
 # =================================================
 
 __author__ = 'patrickczeczko'
@@ -23,7 +23,7 @@ import Scripts.blastprep as blast
 import Scripts.emalPrep as emal
 import Scripts.finalScript as final
 
-
+# Cretaes a txt file of all importnat output files for final report
 def outputFileList(files, projDir):
     outputfile = open(projdir + '6-FinalResult/information/' + 'filelist.txt', 'w+')
     outputfile.write(
@@ -35,21 +35,13 @@ def outputFileList(files, projDir):
                                                                                                           '&#47;') + '</td></tr>')
     outputfile.write('</tbody></table>')
 
-
 # Main Function
 if __name__ == "__main__":
     # Get all user set arguments before starting pipeline
-    print ("Starting EzMap \n\nParsing config file...")
+    print("Starting EzMap \n\nParsing config file...")
     allArgs = arguments.parseCommandLineArguments()
     configOptions = config.parseConfigOptions()
     startStep = int(configOptions['start-at-step'])
-
-    # Create subdirectories where intermediate files will live
-    createdSubDir = fileman.createSubFolders(allArgs['projDir'])
-    # If sub directories don't exist, don't continue
-    if createdSubDir == False:
-        print("Error unable to create intermediate file directories")
-        sys.exit()
 
     # Get list of FASTQ files to process
     origFiles = fileman.getListOfOriginalFiles(allArgs['directory'], allArgs['projDir'])
@@ -60,58 +52,68 @@ if __name__ == "__main__":
         print('Exiting...')
         sys.exit()
 
+    # Create subdirectories where intermediate files will live
+    createdSubDir = fileman.createSubFolders(allArgs['projDir'])
+    # If sub directories don't exist, don't continue
+    if createdSubDir == False:
+        print("Error unable to create intermediate file directories")
+        sys.exit()
+
     # Get the project directory to pass to functions that follow
     projdir = list(origFiles.values())[0].projDirectory
 
     fileman.copyReportFiles(projdir)
 
+    # Generate Job script for step 1 and run all jobs
     prinseqJobIDS = []
     if (startStep == 1):
-        # Generate Job script for step 1 and run all jobs
         prinseq.generateSLURMScript(origFiles, projdir, configOptions)
         prinseqJobIDS = prinseq.processAllFiles(projdir, configOptions, origFiles)
         startStep += 1
 
+    # Generate Job script for step 2 and run all jobs
     bowtie2JobIDS = []
     if (startStep == 2):
-        # Generate Job script for step 2 and run all jobs
         bowtie2.generateSLURMScirpt(origFiles, projdir, configOptions, prinseqJobIDS)
         bowtie2JobIDS = bowtie2.processAllFiles(projdir, configOptions, origFiles)
         startStep += 1
 
+    # Generate Job script for step 3 and run all jobs
     samtoolsJobIDS = []
     if (startStep == 3):
-        # Generate Job script for step 3 and run all jobs
         samtools.generateSLURMScript(origFiles, projdir, configOptions, bowtie2JobIDS)
         samtoolsJobIDS = samtools.processAllFiles(projdir, configOptions, origFiles)
         startStep += 1
 
+    # Generate Job script for step 4 and run all jobs
     blastJobIDS = []
     if (startStep == 4):
-        # Generate Job script for step 4 and run all jobs
         blast.generateSLURMScript(origFiles, projdir, configOptions, samtoolsJobIDS)
         blastJobIDS = blast.processAllFiles(projdir, configOptions, origFiles)
         startStep += 1
 
+    # Generate Job script for step 5 and run all jobs
     emalPreJobIDS = []
     if (startStep == 5):
-        # Generate Job script for step 5 and run all jobs
         emalPre = emal.generatePreScript(origFiles, projdir, configOptions, blastJobIDS)
         emalPreJobIDS = emal.processAllFiles(projdir, configOptions, origFiles, 1, emalPre)
         startStep += 1
 
+    # Generate Job script for step 6 and run all jobs
     emalMainJobIDS = []
     if (startStep == 6):
         emalMain = emal.generateMainScript(origFiles, projdir, configOptions, emalPreJobIDS)
         emalMainJobIDS = emal.processAllFiles(projdir, configOptions, origFiles, 2, emalMain)
         startStep += 1
 
+    # Generate Job script for step 7 and run all jobs
     emalPostJobIDS = []
     if (startStep == 7):
         emalPost = emal.generatePostScript(origFiles, projdir, configOptions, emalMainJobIDS)
         emalPostJobIDS = emal.processAllFiles(projdir, configOptions, origFiles, 3, emalPost)
         startStep += 1
 
+    # Output file list and gather all results throughout the pipeline run and compile into final report
     outputFileList(origFiles, projdir)
     final.collectPipelineResult(configOptions['project-name'], projdir, configOptions, emalPostJobIDS)
 
