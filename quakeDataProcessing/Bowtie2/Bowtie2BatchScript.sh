30e06ae457bb9439935689f4b3d933f8c07c85cd
patrickcze <patrick.cze@gmail.com>
2016-05-10 11:50:35 -0600

    Removed Unessacary files

--- quakeDataProcessing/Bowtie2/Bowtie2BatchScript.sh
+++
@@ -1,7 +0,0 @@
-fileArray=($(python errorCheck.py | tr -d '[],'))
-
-numOfFiles=${#fileArray[@]}
-echo Starting Bowtie2 Processing
-echo Number of Files: ${numOfFiles}
-
-sbatch --array=0-$((numOfFiles-1)) Bowtie2MappingScript.sh
