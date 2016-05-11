636d82e2b9f598a816ac4906cc464a4acb22cf67
patrickcze <patrick.cze@gmail.com>
2016-05-11 14:28:28 -0600

    Added the ability to view more than one EzMap Report at the same time.

--- EzMap/ReportFiles/viewResults.py
+++ EzMap/ReportFiles/viewResults.py
@@ -2,18 +2,23 @@ import SimpleHTTPServer
 import SocketServer
 import webbrowser
 import atexit
-
-PORT = 8000
-
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-
-httpd = SocketServer.TCPServer(("", PORT), Handler)
-
-print "serving at port", PORT
-webbrowser.open('http://localhost:8000/report.html')
-httpd.serve_forever()
-
-atexit.register(exit_handler(httpd))
+import random
 
 def exit_handler(httpd):
     httpd.shutdown()
+
+def accessReport (PORT):
+    try:
+        Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+        httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+        print "serving at port", PORT
+        webbrowser.open('http://localhost:' + str(PORT) + '/report.html')
+        httpd.serve_forever()
+
+        atexit.register(exit_handler(httpd))
+    except SocketServer.socket.error as e:
+        accessReport(PORT+1)
+
+PORT = 8000
+accessReport(PORT)
