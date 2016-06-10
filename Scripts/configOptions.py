9f67ddd4999d36ea23aa333ebd3056f871a139fe
patrickcze <patrick.cze@gmail.com>
2016-06-10 14:30:22 -0600

    Changed the name of the config files to no longer contain .config as their extension. Replaced # with > for each config option in the pipeline

--- Scripts/configOptions.py
+++ Scripts/configOptions.py
@@ -5,11 +5,11 @@ import os
 # Parses the param.config file to obtain all of the options for each of the pipeline steps
 def parseConfigOptions():
     cwd = os.path.dirname(os.path.abspath(__file__)).strip('Scripts')
-    configFile = open(cwd + 'param.config', 'r')
+    configFile = open(cwd + 'config.txt', 'r')
 
     configOptions = {}
     for line in configFile:
-        if line.startswith('#'):
+        if line.startswith('>'):
             option = line.split('=')
             option[1] = option[1].replace('\n', '')
             configOptions[option[0][1:]] = option[1]
