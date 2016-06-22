de529ba8ef0c3ecd10c7c958f9da3651b55a9c19
patrickcze <patrick.cze@gmail.com>
2016-06-22 14:02:54 -0600

    Added tooltips

--- SampleReport/WEB/js/sequences.js
+++ SampleReport/WEB/js/sequences.js
@@ -1,45 +1,55 @@
 // Dimensions of sunburst.
-var width = (($( document ).width()-5)/12)*8;
-var height = $(window).height()/12*8;
+var width = (($(document).width() - 5) / 12) * 8;
+var height = $(window).height() / 12 * 8;
 var radius = Math.min(width, height) / 2;
 
 var file = "../../information/emal-graph-1.csv";
 
-var colorScale = ['#3182bd','#6baed6','#9ecae1','#c6dbef','#e6550d','#fd8d3c','#fdae6b','#fdd0a2','#31a354','#74c476','#a1d99b','#c7e9c0','#756bb1','#9e9ac8','#bcbddc','#dadaeb','#636363','#969696','#bdbdbd','#d9d9d9']
+var colorScale = ['#3182bd', '#6baed6', '#9ecae1', '#c6dbef', '#e6550d', '#fd8d3c', '#fdae6b', '#fdd0a2', '#31a354', '#74c476', '#a1d99b', '#c7e9c0', '#756bb1', '#9e9ac8', '#bcbddc', '#dadaeb', '#636363', '#969696', '#bdbdbd', '#d9d9d9']
 
-function colorMaker (x){
-    var selectedColor = (((x.charCodeAt(0)+x.charCodeAt(1)+x.charCodeAt(2)))%colorScale.length)
+function colorMaker(x) {
+    var selectedColor = (((x.charCodeAt(0) + x.charCodeAt(1) + x.charCodeAt(2))) % colorScale.length)
     return colorScale[selectedColor];
 }
 
 // Total size of all segments; we set this later, after loading the data.
-var totalSize = 0; 
+var totalSize = 0;
 
 var vis = d3.select("#chart").append("svg:svg")
-.attr("width", width)
-.attr("height", height)
-.append("svg:g")
-.attr("id", "GRAContainer")
-.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    .attr("width", width)
+    .attr("height", height)
+    .append("svg:g")
+    .attr("id", "GRAContainer")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 var partition = d3.layout.partition()
-.size([2 * Math.PI, radius * radius])
-.value(function(d) { return d.size; });
+    .size([2 * Math.PI, radius * radius])
+    .value(function (d) {
+        return d.size;
+    });
 
 var arc = d3.svg.arc()
-.startAngle(function(d) { return d.x; })
-.endAngle(function(d) { return d.x + d.dx; })
-.innerRadius(function(d) { return Math.sqrt(d.y); })
-.outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
+    .startAngle(function (d) {
+        return d.x;
+    })
+    .endAngle(function (d) {
+        return d.x + d.dx;
+    })
+    .innerRadius(function (d) {
+        return Math.sqrt(d.y);
+    })
+    .outerRadius(function (d) {
+        return Math.sqrt(d.y + d.dy);
+    });
 
-d3.json("information/emal-graph-1.json", function(data2) {
-    console.log(data2);
+d3.json("information/emal-graph-1.json", function (data2) {
+    //    console.log(data2);
     var newArray = []
-    for (key in data2){
-        console.log(data2[key]["Taxonomy"])
-        newArray.push([data2[key]["Taxonomy"],data2[key]["GRA"]]);
+    for (key in data2) {
+        //        console.log(data2[key]["Taxonomy"])
+        newArray.push([data2[key]["Taxonomy"], data2[key]["GRA"]]);
     }
-    console.log(newArray)
+    //    console.log(newArray)
     var json = buildHierarchy(newArray);
     createVisualization(json);
 });
@@ -61,38 +71,42 @@ function createVisualization(json) {
 
     // For efficiency, filter nodes to keep only those large enough to see.
     var nodes = partition.nodes(json)
-    .filter(function(d) {
-        return (d.dx > 0.005); // 0.005 radians = 0.29 degrees
-    });
+        .filter(function (d) {
+            return (d.dx > 0.005); // 0.005 radians = 0.29 degrees
+        });
 
-//    console.log("nodes",JSON.stringify(nodes));
-    
+    //    console.log("nodes",JSON.stringify(nodes));
+
     var path = vis.data([json]).selectAll("path")
-    .data(nodes)
-    .enter().append("svg:path")
-    .attr("display", function(d) { return d.depth ? null : "none"; })
-    .attr("d", arc)
-    .attr("fill-rule", "evenodd")
-    .style("fill", function(d) { return colorMaker(d.name); })
-    .style("opacity", 1)
-    .on("mouseover", mouseover);
-    
-    var svgWidth = d3.select('#chart').select("svg").style('height').replace('px','');
+        .data(nodes)
+        .enter().append("svg:path")
+        .attr("display", function (d) {
+            return d.depth ? null : "none";
+        })
+        .attr("d", arc)
+        .attr("fill-rule", "evenodd")
+        .style("fill", function (d) {
+            return colorMaker(d.name);
+        })
+        .style("opacity", 1)
+        .on("mouseover", mouseover);
+
+    var svgWidth = d3.select('#chart').select("svg").style('height').replace('px', '');
     var textHeight = 60;
-    var topValue = (textHeight/-2);
-    
+    var topValue = (textHeight / -2);
+
     var explanation = vis.append("svg:text")
-    .attr('id','explanation')
-    .attr('y',topValue)
-    .attr("text-anchor","middle")
-    .data("TESTING");
-    
+        .attr('id', 'explanation')
+        .attr('y', topValue)
+        .attr("text-anchor", "middle")
+        .data("TESTING");
+
     totalSize = path.node().__data__.value;
 };
 
 // Fade all but the current sequence, and show it in the breadcrumb trail.
 function mouseover(d) {
-    if (this.nodeName == 'path'){
+    if (this.nodeName == 'path') {
         var percentage = (100 * d.value / totalSize).toPrecision(3);
         var percentageString = percentage + "%";
         if (percentage < 0.01) {
@@ -104,7 +118,7 @@ function mouseover(d) {
         var last_element = sequenceArray[sequenceArray.length - 1];
 
         d3.select('#explanation')
-        .html('');
+            .html('');
 
         d3.select('#explanation')
             .attr('class', '')
@@ -116,7 +130,7 @@ function mouseover(d) {
             .append('svg:tspan')
             .attr('x', 0)
             .attr('dy', 30)
-            .attr('id','percentageString')
+            .attr('id', 'percentageString')
             .text(percentageString);
         d3.select('#explanation')
             .append('svg:tspan')
@@ -131,28 +145,30 @@ function mouseover(d) {
 
         // Then highlight only those that are an ancestor of the current segment.
         vis.selectAll("path")
-            .filter(function(node) {
-            return (sequenceArray.indexOf(node) >= 0);
-        })
+            .filter(function (node) {
+                return (sequenceArray.indexOf(node) >= 0);
+            })
             .style("opacity", 1);
     }
 }
 
-function updateTable(array){
+function updateTable(array) {
     var tableString = "<thead><tr><th>Classifier</th><th>Name</th></tr></thead><tbody id='emal-grph-1-tbl-tbody'>";
-    while (array.length < 7){
-        array.push({name:''})
+    while (array.length < 7) {
+        array.push({
+            name: ''
+        })
     }
-    
-    tableString += '<tr><td>Super Kingdom</td><td>'+array[0].name+'</td></tr>';
-    tableString += '<tr><td>Q1</td><td>'+array[1].name+'</td></tr>';
-    tableString += '<tr><td>Order</td><td>'+array[2].name+'</td></tr>';
-    tableString += '<tr><td>Family</td><td>'+array[3].name+'</td></tr>';
-    tableString += '<tr><td>Sub Family</td><td>'+array[4].name+'</td></tr>';
-    tableString += '<tr><td>Genus</td><td>'+array[5].name+'</td></tr>';
-    tableString += '<tr><td>Species</td><td>'+array[6].name+'</td></tr>';
 
-    tableString+='</tbody>';
+    tableString += '<tr><td>Super Kingdom</td><td>' + array[0].name + '</td></tr>';
+    tableString += '<tr><td>Q1</td><td>' + array[1].name + '</td></tr>';
+    tableString += '<tr><td>Order</td><td>' + array[2].name + '</td></tr>';
+    tableString += '<tr><td>Family</td><td>' + array[3].name + '</td></tr>';
+    tableString += '<tr><td>Sub Family</td><td>' + array[4].name + '</td></tr>';
+    tableString += '<tr><td>Genus</td><td>' + array[5].name + '</td></tr>';
+    tableString += '<tr><td>Species</td><td>' + array[6].name + '</td></tr>';
+
+    tableString += '</tbody>';
 
     $('#emal-grph-1-tbl').html(tableString);
     //$('#emal-grph-1-tbl').css('width',((($( document ).width()-5)/12)*4));
@@ -175,7 +191,10 @@ function getAncestors(node) {
 // root to leaf, separated by hyphens. The second column is a count of how 
 // often that sequence occurred.
 function buildHierarchy(csv) {
-    var root = {"name": "root", "children": []};
+    var root = {
+        "name": "root"
+        , "children": []
+    };
     for (var i = 0; i < csv.length; i++) {
         var sequence = csv[i][0];
         var size = +csv[i][1];
@@ -200,13 +219,19 @@ function buildHierarchy(csv) {
                 }
                 // If we don't already have a child node for this branch, create it.
                 if (!foundChild) {
-                    childNode = {"name": nodeName, "children": []};
+                    childNode = {
+                        "name": nodeName
+                        , "children": []
+                    };
                     children.push(childNode);
                 }
                 currentNode = childNode;
             } else {
                 // Reached the end of the sequence; create a leaf node.
-                childNode = {"name": nodeName, "size": size};
+                childNode = {
+                    "name": nodeName
+                    , "size": size
+                };
                 children.push(childNode);
             }
         }
