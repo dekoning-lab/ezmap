89f323452236eeaac7970b9b69bd71029a2d973b
patrickcze <patrick.cze@gmail.com>
2016-06-30 14:33:59 -0600

    Simplified the execution of the results gathering script

--- Scripts/finalScript.py
+++ Scripts/finalScript.py
@@ -6,21 +6,9 @@ import Scripts.slurmScript as slurmScript
 def collectPipelineResult(projName, projDir, configOptions, finalJobIDS):
     cwd = os.path.dirname(os.path.abspath(__file__)).strip('Scripts')
 
-    script = open(projDir + '6-FinalResult-' + projName + '/information/resultsScript.sh', 'w+')
-
-    slurmScript.getSBATCHSettings(script, 'FINALRESULTS', projDir + '6-FinalResult/', configOptions)
-
     IDList = ''
     for i in finalJobIDS:
         IDList += ':' + str(i)
 
-    if finalJobIDS:
-        script.write('#SBATCH --dependency=afterany:' + IDList[1:] + '\n\n')
-
-    script.write(
-        configOptions['python3-path'] + ' ' + cwd + 'Scripts/gatherResults.py ' + projDir + ' ' + projName)
-
-    command = 'sbatch -p ' + configOptions['slurm-partition'] + ' -J EZMAP-FINALRESULTS --wrap "sh ' + os.path.abspath(
-        projDir) + '/6-FinalResult-' + projName + '/information/resultsScript.sh' + '"'
-
+    command = 'sbatch -J EZMAP-FINALRESULTS -p '+configOptions['slurm-partition']+' --dependency=afterany:' + IDList[1:] + ' --wrap="'+configOptions['python3-path'] + ' ' + cwd + 'Scripts/gatherResults.py ' + projDir + ' ' + projName+'"'
     os.system(command)
